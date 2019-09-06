0fae7f75e75babf9a43751c8ddc7a25f1d5262e5
Kris P Sherwin <43814048+krispy59@users.noreply.github.com>
2019-09-06 15:41:19 -0500

    Create ngrk.pp

---
+++ site-modules/profile/manifests/ngrk.pp
@@ -0,0 +1,10 @@
+class ngrk {
+  # Uses the gabe-ngrok forge module to make a tunnel for CD's webhook.
+  class { 'ngrok':
+    authtoken => '1QTfseR4XbIekJjzHW2tW4q0nx9_2A4qeAgfa2S7cpCaWxNbb',
+  }
+  ngrok::tunnel { 'cd4pe':
+    proto => 'tcp',
+    addr  => '8000',
+  }
+}
